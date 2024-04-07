5f010cde2db7b918d62f2e87c61fb0795ccb4193
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-07 16:01:29 +0600

    Fix Ring Progress Bar container screen.

--- LetsTools/LetsRingProgressBar/RingProgressBarScreen.swift
+++ LetsTools/LetsRingProgressBar/RingProgressBarScreen.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct RingProgressBarScreen: View {
+    @Environment(\.dismiss) var dismiss
+
     @State var drawingStroke: Bool = false
     @State var progressValueOne: CGFloat = 0.0
     @State var progressValueTwo: CGFloat = 0.0
@@ -26,6 +28,18 @@ struct RingProgressBarScreen: View {
                 progressValueOne = CGFloat.random(in: 0...1)
                 progressValueTwo = CGFloat.random(in: 0...1)
                 progressValueThree = CGFloat.random(in: 0...1)
+            }
+            .navigationTitle("Ring Progress Bar")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                    }
+                }
             }
             .ignoresSafeArea()
         }
