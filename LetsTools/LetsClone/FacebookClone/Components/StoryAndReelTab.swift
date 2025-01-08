bc6e0eab89318774f0780c2bb122c175611e7733
Maraj Hossain <marajhossaincse@gmail.com>
2025-01-08 13:07:25 +0600

    Add line below tab text in reels and stories.

--- LetsTools/LetsClone/FacebookClone/Components/StoryAndReelTab.swift
+++ LetsTools/LetsClone/FacebookClone/Components/StoryAndReelTab.swift
@@ -23,6 +23,12 @@ struct StoryAndReelTab: View {
                         .padding()
                         .foregroundColor(selectedTab == 0 ? .blue : .gray)
                         .frame(maxWidth: .infinity)
+                        .overlay(
+                            Rectangle()
+                                .frame(width: 160, height: 2)
+                                .foregroundColor(selectedTab == 0 ? .blue : .white),
+                            alignment: .bottom
+                        )
                 }
 
                 Button(
@@ -35,6 +41,12 @@ struct StoryAndReelTab: View {
                         .padding()
                         .foregroundColor(selectedTab == 1 ? .blue : .gray)
                         .frame(maxWidth: .infinity)
+                        .overlay(
+                            Rectangle()
+                                .frame(width: 155, height: 2)
+                                .foregroundColor(selectedTab == 1 ? .blue : .white),
+                            alignment: .bottom
+                        )
                 }
             }
             .padding(.top)
