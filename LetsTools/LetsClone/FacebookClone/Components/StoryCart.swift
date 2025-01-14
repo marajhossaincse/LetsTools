5b87a547f844b236d1addbd1aeb55204985a2b08
Maraj Hossain <marajhossaincse@gmail.com>
2025-01-14 12:56:56 +0600

    Remove comments and lint code.

--- LetsTools/LetsClone/FacebookClone/Components/StoryCart.swift
+++ LetsTools/LetsClone/FacebookClone/Components/StoryCart.swift
@@ -1,27 +1,50 @@
 import SwiftUI
 
 struct StoryCart: View {
-    
-    
-    let users = [
-        ("John", "https://picsum.photos/id/10/200/300", true),
-        ("Anna", "https://picsum.photos/id/11/200/300", false),
-        ("Mark", "https://picsum.photos/id/12/200/300", true),
-        ("Grace", "https://picsum.photos/id/13/200/300", true),
-        ("Mike", "https://picsum.photos/id/14/200/300", true)
+    let users: [User] = [
+        User(
+            name: "John",
+            profileImage: "https://picsum.photos/200",
+            storyImage: "https://picsum.photos/id/10/200/300",
+            isOnline: true
+        ),
+        User(
+            name: "Anna",
+            profileImage: "https://picsum.photos/200",
+            storyImage: "https://picsum.photos/id/11/200/300",
+            isOnline: false
+        ),
+        User(
+            name: "Mark",
+            profileImage: "https://picsum.photos/200",
+            storyImage: "https://picsum.photos/id/12/200/300",
+            isOnline: false
+        ),
+        User(
+            name: "Grace",
+            profileImage: "https://picsum.photos/200",
+            storyImage: "https://picsum.photos/id/13/200/300",
+            isOnline: true
+        ),
+        User(
+            name: "Mike",
+            profileImage: "https://picsum.photos/200",
+            storyImage: "https://picsum.photos/id/14/200/300",
+            isOnline: true
+        )
     ]
 
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 20) {
-                ForEach(users, id: \.0) { user in
+                ForEach(users) { user in
                     ZStack(alignment: .topLeading) {
                         // Story Image
                         RoundedRectangle(cornerRadius: 10)
                             .fill(Color.gray.opacity(0.1))
                             .frame(width: 155, height: 250)
 
-                        AsyncImage(url: URL(string: user.1)) { image in
+                        AsyncImage(url: URL(string: user.storyImage)) { image in
                             image.resizable()
                                 .scaledToFill()
                                 .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -33,7 +56,7 @@ struct StoryCart: View {
 
                         ZStack(alignment: .center) {
                             // Profile Image
-                            AsyncImage(url: URL(string: "https://picsum.photos/200")) { profileImage in
+                            AsyncImage(url: URL(string: user.profileImage)) { profileImage in
                                 profileImage
                                     .resizable()
                                     .scaledToFill()
@@ -47,7 +70,7 @@ struct StoryCart: View {
                                     .overlay(Circle())
                             }
 
-                            if user.2 {
+                            if user.isOnline {
                                 Circle()
                                     .strokeBorder(.white)
                                     .background(Circle().fill(.green))
--- LetsTools/LetsClone/FacebookClone/Models/User.swift
+++ LetsTools/LetsClone/FacebookClone/Models/User.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class User: Identifiable {
+    var id: UUID
+    let name: String
+    let profileImage: String
+    let storyImage: String
+    let isOnline: Bool
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        profileImage: String,
+        storyImage: String,
+        isOnline: Bool
+    ) {
+        self.id = id
+        self.name = name
+        self.profileImage = profileImage
+        self.storyImage = storyImage
+        self.isOnline = isOnline
+    }
+}
