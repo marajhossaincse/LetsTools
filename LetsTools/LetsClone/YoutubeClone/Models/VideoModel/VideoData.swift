2ccb3a852fe591836680900129a1eee8f39fe318
Maraj Hossain <marajhossaincse@gmail.com>
2025-01-31 19:59:24 +0600

    Add home view components for youtube clone.

---
+++ LetsTools/LetsClone/YoutubeClone/Models/VideoModel/VideoData.swift
@@ -0,0 +1,8 @@
+//
+//  VideoData.swift
+//  LetsTools
+//
+//  Created by Maraz Hossain on 1/31/25.
+//
+
+import Foundation
---
+++ LetsTools/LetsClone/YoutubeClone/Models/VideoModel/VideoResponse.swift
@@ -0,0 +1,8 @@
+//
+//  VideoResponse.swift
+//  LetsTools
+//
+//  Created by Maraz Hossain on 1/31/25.
+//
+
+import Foundation
