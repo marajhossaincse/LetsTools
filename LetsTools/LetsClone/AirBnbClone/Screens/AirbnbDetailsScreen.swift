32ebac550142774df5456d29fb8b71182b180cdd
Maraj Hossain <marajhossaincse@gmail.com>
2025-02-10 19:37:29 +0600

    Add toolbar for bnb details screen.

---
+++ LetsTools/LetsClone/AirBnbClone/Screens/AirbnbDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AirbnbDetailsScreen.swift
+//  LetsTools
+//
+//  Created by Maraz Hossain on 2/9/25.
+//
+
+import SwiftUI
+
+struct AirbnbDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AirbnbDetailsScreen()
+}
