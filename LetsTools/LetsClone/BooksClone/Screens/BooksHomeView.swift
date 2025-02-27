f06b62135f8c9dc20569866a45bee6ef32df457c
Maraj Hossain <marajhossaincse@gmail.com>
2025-02-27 22:54:58 +0600

    feat: Add top chart section.

--- LetsTools/LetsClone/BooksClone/Screens/BooksHomeView.swift
+++ LetsTools/LetsClone/BooksClone/Screens/BooksHomeView.swift
@@ -13,14 +13,17 @@ struct BooksHomeView: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
+            ScrollView(showsIndicators: false) {
                 VStack(spacing: 0) {
                     ThemeBasedBookBannerView()
 
                     NewAndTrendingBooksView()
+
+                    TopChartsView()
                 }
             }
             .navigationTitle("Book Store")
+            .background(Color.secondarySystemBackground)
         }
     }
 }
@@ -91,7 +94,7 @@ struct NewAndTrendingBooksView: View {
                 .fontWeight(.semibold)
 
             Text("Recently released and notable books.")
-                .font(.caption)
+                .font(.subheadline)
 
             ScrollView(.horizontal, showsIndicators: false) {
                 LazyHGrid(rows: gridItems, spacing: 10) {
@@ -106,5 +109,70 @@ struct NewAndTrendingBooksView: View {
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.leading)
         .padding(.vertical)
+    }
+}
+
+struct TopChartsView: View {
+    var books: [BookResponse] = booksData
+
+    let gridItems = [
+        GridItem(.fixed(150)),
+        GridItem(.fixed(150)),
+        GridItem(.fixed(150))
+    ]
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Top Charts")
+                .font(.title2)
+                .fontWeight(.semibold)
+
+            Divider()
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHGrid(rows: gridItems, spacing: 32) {
+                    ForEach(books) { book in
+                        TopChartRowView(book: book)
+                    }
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading)
+    }
+}
+
+struct TopChartRowView: View {
+    var book: BookResponse
+
+    var body: some View {
+        VStack {
+            HStack(spacing: 15) {
+                KFImage(URL(string: book.thumbnail))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 110)
+
+                Text("1")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+
+                VStack(alignment: .leading) {
+                    Text(book.title)
+                        .font(.headline)
+                        .fontWeight(.semibold)
+
+                    Text(book.author)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.systemGray)
+                }
+                .padding(.trailing)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+
+            Divider()
+                .padding(.vertical)
+        }
     }
 }
